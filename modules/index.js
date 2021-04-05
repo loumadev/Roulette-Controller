ef87632a0c17e99433d5581d879606d047d1cb22
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2021-04-05 14:01:18 +0200

    initial commit

---
+++ modules/index.js
@@ -0,0 +1,177 @@
+const {Server, editJSON} = require("../server.js");
+const fs = require("fs");
+const Gpio = require("onoff").Gpio;
+const {getUniqueID} = require("../JustLib.js");
+
+const PATH_CONFIG = __dirname + "/config.json";
+const DEFAULT_CONFIG = {
+	NAME: "Some name",
+	START: {h: 18, m: 32},
+	STOP: {h: 18, m: 34},
+	DELAY: 5000,
+	IMPULSE: 500,
+	LOCKED: null
+};
+
+/*const RELAY = {
+	START: new Gpio(4, "out"),
+	STOP: new Gpio(5, "out")
+};*/
+
+Server.on("load", e => {
+	Roulette.init();
+	Roulette.getState();
+});
+
+Server.on("/", e => {
+	e.redirect("/page.html");
+});
+
+Server.on("/api/config", e => {
+	e.send({
+		"success": true,
+		"config": Roulette.config,
+		"state": Roulette.state
+	});
+});
+
+Server.on("/api/toggle", e => {
+	const {state} = e.query;
+
+	Roulette.sendSignal(state);
+
+	e.send({
+		"success": true,
+		"state": Roulette.state
+	});
+});
+
+Server.on("/api/update", e => {
+	e.post(config => {
+		Roulette.config = config;
+		Roulette.saveConfig();
+
+		e.send({
+			"success": true,
+			"config": Roulette.config,
+			"state": Roulette.state
+		});
+	}, "json");
+});
+
+class Roulette {
+	static config = null;
+	static state = null;
+	static middleState = null;
+	static session = null;
+
+	static init() {
+		/*if(!fs.existsSync(PATH_CONFIG)) {
+			fs.writeFileSync(PATH_CONFIG, JSON.stringify(DEFAULT_CONFIG, null, "\t"));
+		}
+
+		this.config = JSON.parse(fs.readFileSync(PATH_CONFIG).toString());*/
+
+		this.config = DEFAULT_CONFIG;
+
+		/*var t = new Date("2021.02.14 12:57");
+		console.log(t);
+		executeAt(t, () => {
+			console.log("Executed!!!");
+		});*/
+	}
+
+	static sendSignal(state = false) {
+		if(this.config.LOCKED !== null) {
+			state = this.config.LOCKED;
+		}
+
+		this.state = state;
+		Server.log(`Sending signal to ` + state);
+	}
+
+	static getState(state = 0) {
+		const session = getUniqueID(24);
+		this.session = session;
+
+		var now = new Date(/*"2021.02.16. 18:00"*/);
+		now.setSeconds(0, 0);
+
+		var start = new Date(now); start.setHours(this.config.START.h, this.config.START.m);
+		var stop = new Date(now); stop.setHours(this.config.STOP.h, this.config.STOP.m);
+
+		fixTime(now);
+		fixTime(start);
+		fixTime(stop);
+
+		//On boot
+		if(state == 0) {
+			state = isInRange(now, start, stop) ? 1 : 2;
+			if(state == 1) {
+				console.log("running");
+				setTimeout(() => {
+					if(session != this.session) return console.log("Newer session found, ignoring this one.");
+					this.sendSignal(true);
+				}, this.config.DELAY);
+			}
+			if(state == 2) {
+				console.log("stopped");
+				setTimeout(() => {
+					if(session != this.session) return console.log("Newer session found, ignoring this one.");
+					this.sendSignal(false);
+				}, this.config.DELAY);	//Is it really required?
+			}
+		}
+
+		if(start <= now) start.setHours(start.getHours() + 24);
+		if(stop <= now) stop.setHours(stop.getHours() + 24);
+
+		console.log(now, ":", start, stop);
+
+		if(state == 1) {
+			executeAt(stop, () => {
+				if(session != this.session) return console.log("Newer session found, ignoring this one.");
+				console.log("stopping...");
+				this.sendSignal(false);
+				this.getState(2);
+			});
+		}
+		if(state == 2) {
+			executeAt(start, () => {
+				if(session != this.session) return console.log("Newer session found, ignoring this one.");
+				console.log("starting...");
+				this.sendSignal(true);
+				this.getState(1);
+			});
+		}
+	}
+
+	static saveConfig() {
+		editJSON(PATH_CONFIG, json => {
+			json = this.config;
+			return json;
+		});
+	}
+}
+
+
+function fixTime(date) {
+	date.setTime(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
+}
+
+function executeAt(time, callback) {
+	var t = new Date(); fixTime(t);
+	if(t > time) {
+		console.error("Invalid time");
+		return false;
+	}
+	console.log(time - t, time, t, "Timeout");
+	setTimeout(callback, time - t);
+	return true;
+}
+
+function isInRange(date, from, to) {
+	if(to < from) to.setHours(to.getHours() + 24);
+
+	return date >= from && date <= to;
+}
---
+++ public/script.js
@@ -0,0 +1,147 @@
+const PLACES = [
+	{
+		name: "Golden Royal Club",
+		host: "/",
+		config: null
+	},
+	{
+		name: "Club 17",
+		host: "",
+		config: null
+	}
+];
+
+const ELM = {
+	LOCK_SELECT: JL(".lock select"),
+	LOCK_CHECKBOX: JL(".lock input"),
+};
+
+class API {
+	static async getConfig(host = "/") {
+		return await this._send(`${host}api/config`);
+	}
+
+	static async toggleState(state, host = "/") {
+		return await this._send(`${host}api/toggle?state=${state}`);
+	}
+
+	static async updateConfig(config, host = "/") {
+		return await this._send(`${host}api/update`, config);
+	}
+
+	static async _send(url, data = null) {
+		return await fetch(url, {
+			method: data ? "POST" : "GET",
+			body: data ? JSON.stringify(data) : null
+		})
+			.then(res => res.json())
+			.catch(err => {
+				console.error(err);
+				return {"success": false, "error": err};
+			})
+			.then(json => {
+				if(!json.success) alert("Error: " + json.error);
+				return json;
+			});
+	}
+}
+
+async function main() {
+	await loadConfig();
+
+	updatePlaces();
+
+}
+
+function generatePlace(place) {
+	const node = parseHTML(`<section class="table column" data-name="${place.name}">
+		<article class="title table column">
+			<span>${place.name}</span>
+		</article>
+		<article class="schedule table column">
+			<label>Schedule</label>
+			<div class="item table row">
+				<span title="Starting time of the schedule">Start:</span>
+				<input class="start" type="time" step="60" value="${place.config.START.h}:${place.config.START.m}">
+			</div>
+			<div class="item table row">
+				<span title="Ending time of the schedule">Stop:</span>
+				<input class="stop" type="time" step="60" value="${place.config.STOP.h}:${place.config.STOP.m}">
+			</div>
+			<div class="item table row">
+				<span title="Time in SECONDS between shutdown and bootup">Delay:</span>
+				<input class="delay" type="number" step="1" min="0" max="3600" value="${Math.floor(place.config.DELAY / 1000)}">
+			</div>
+		</article>
+		<article class="lock table column">
+			<label>Lock state</label>
+			<div class="item table row v-center">
+				<select value="${place.config.LOCKED}" title="Select state to lock">
+					<option value="null" selected disabled hidden>Select</option>
+					<option value="true">Running</option>
+					<option value="false">Stopped</option>
+				</select>
+				<input type="checkbox" id="lock" ${place.config.LOCKED == null ? "" : "checked"}>
+				<label for="lock" class="table center" title="Lock selected state">
+					<svg viewBox="0 0 576 512" style="margin-left: 4px" class="unlocked">
+						<path fill="currentColor" d="M423.5 0C339.5.3 272 69.5 272 153.5V224H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48h-48v-71.1c0-39.6 31.7-72.5 71.3-72.9 40-.4 72.7 32.1 72.7 72v80c0 13.3 10.7 24 24 24h32c13.3 0 24-10.7 24-24v-80C576 68 507.5-.3 423.5 0zM264 392c0 22.1-17.9 40-40 40s-40-17.9-40-40v-48c0-22.1 17.9-40 40-40s40 17.9 40 40v48z"></path>
+					</svg>
+					<svg viewBox="0 0 448 512" class="locked">
+						<path fill="currentColor" d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zM264 392c0 22.1-17.9 40-40 40s-40-17.9-40-40v-48c0-22.1 17.9-40 40-40s40 17.9 40 40v48zm32-168H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"></path>
+					</svg>
+				</label>
+			</div>
+		</article>
+		<article class="manual table column">
+			<label>Manual control</label>
+			<div class="item table row">
+				<button class="start secondary" onclick="confirm('Do you really want to turn the roullete ON (${place.name})?') && API.toggleState(true, '${place.host}')" title="Send signal to start the roulette manually">Start</button>
+				<button class="stop secondary" onclick="confirm('Do you really want to turn the roullete OFF (${place.name})?') && API.toggleState(false, '${place.host}')" title="Send signal to stop the roulette manually">Stop</button>
+				<button class="apply primary" onclick="confirm('Do you really want to save all changed values (${place.name})?') && saveChanges('${place.name}')" title="Save changes">Apply</button>
+			</div>
+		</article>
+	</section>`);
+
+	return node;
+}
+
+async function saveChanges(name) {
+	const place = PLACES.find(e => e.name == name);
+	const section = JL(`section[data-name="${place.name}"]`);
+
+	const start = JL(section, "input.start").value.split(":");
+	const stop = JL(section, "input.stop").value.split(":");
+	const delay = JL(section, "input.delay").value;
+	const locked = JL(section, `.lock input[type="checkbox"]`).checked;
+	const lockState = JL(section, `.lock select`).value;
+
+	place.config.START.h = +start[0];
+	place.config.START.m = +start[1];
+
+	place.config.STOP.h = +stop[0];
+	place.config.STOP.m = +stop[1];
+
+	place.config.DELAY = +delay * 1000;
+	place.config.LOCKED = locked ? lockState : null;
+
+	const res = await API.updateConfig(place.config, place.host);
+	console.log(res);
+}
+
+function updatePlaces() {
+	JL("main").innerHTML = "";
+
+	for(const place of PLACES) {
+		JL("main").appendChild(generatePlace(place));
+	}
+}
+
+async function loadConfig() {
+	for(const place of PLACES) {
+		console.log(`Loading configuration of ${place.name}...`);
+		place.config = (await API.getConfig(place.host)).config;
+		console.log("Configuration loaded", place.config);
+	}
+}
+
+main();
