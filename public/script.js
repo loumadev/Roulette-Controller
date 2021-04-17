d75d1166e4154f361c3b40b17317ff5e92f3c1b8
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2021-04-17 19:45:09 +0200

    Switched to single place per session

--- public/script.js
+++ public/script.js
@@ -1,17 +1,7 @@
-const PLACES = [
-	{
-		name: "Golden Royal Club",
-		host: "/",
-		config: null,
-		state: null
-	},
-	{
-		name: "Club 17",
-		host: "",
-		config: null,
-		state: null
-	}
-];
+const PLACE = {
+	config: null,
+	state: null
+};
 
 const STATE = {
 	BOOT: 0,
@@ -25,16 +15,16 @@ const ELM = {
 };
 
 class API {
-	static async getConfig(host = "/") {
-		return await this._send(`${host}api/config`);
+	static async getConfig() {
+		return await this._send(`/api/config`);
 	}
 
-	static async toggleState(state, host = "/") {
-		return await this._send(`${host}api/toggle?state=${state}`);
+	static async toggleState(state) {
+		return await this._send(`/api/toggle?state=${state}`);
 	}
 
-	static async updateConfig(config, host = "/") {
-		return await this._send(`${host}api/update`, config);
+	static async updateConfig(config) {
+		return await this._send(`/api/update`, config);
 	}
 
 	static async _send(url, data = null) {
@@ -58,39 +48,38 @@ async function main() {
 	await loadConfig();
 
 	updatePlaces();
-
 }
 
-function generatePlace(place) {
-	const node = parseHTML(`<section class="table column" data-name="${place.name}">
+function generatePlace() {
+	const node = parseHTML(`<section class="table column" data-name="${PLACE.config.NAME}">
 		<article class="title table row v-center">
-			<span>${place.name}</span>
-			<div class="state" data-state="${place.state}"></div>
+			<span>${PLACE.config.NAME}</span>
+			<div class="state" data-state="${PLACE.state}"></div>
 		</article>
 		<article class="schedule table column">
 			<label>Schedule</label>
 			<div class="item table row">
 				<span title="Starting time of the schedule">Start:</span>
-				<input class="start" type="time" step="60" value="${fixDigits(place.config.START.h)}:${fixDigits(place.config.START.m)}">
+				<input class="start" type="time" step="60" value="${fixDigits(PLACE.config.START.h)}:${fixDigits(PLACE.config.START.m)}">
 			</div>
 			<div class="item table row">
 				<span title="Ending time of the schedule">Stop:</span>
-				<input class="stop" type="time" step="60" value="${fixDigits(place.config.STOP.h)}:${fixDigits(place.config.STOP.m)}">
+				<input class="stop" type="time" step="60" value="${fixDigits(PLACE.config.STOP.h)}:${fixDigits(PLACE.config.STOP.m)}">
 			</div>
 			<div class="item table row">
 				<span title="Time in SECONDS between shutdown and bootup">Delay:</span>
-				<input class="delay" type="number" step="1" min="0" max="3600" value="${Math.floor(place.config.DELAY / 1000)}">
+				<input class="delay" type="number" step="1" min="0" max="3600" value="${Math.floor(PLACE.config.DELAY / 1000)}">
 			</div>
 		</article>
 		<article class="lock table column">
 			<label>Lock state</label>
 			<div class="item table row v-center">
-				<select value="${place.config.LOCKED}" title="Select state to lock">
+				<select value="${PLACE.config.LOCKED}" title="Select state to lock">
 					<option value="null" disabled hidden>Select</option>
 					<option value="1">Running</option>
 					<option value="2">Stopped</option>
 				</select>
-				<input type="checkbox" id="lock" ${place.config.LOCKED == null ? "" : "checked"}>
+				<input type="checkbox" id="lock" ${PLACE.config.LOCKED == null ? "" : "checked"}>
 				<label for="lock" class="table center" title="Lock selected state">
 					<svg viewBox="0 0 576 512" style="margin-left: 4px" class="unlocked">
 						<path fill="currentColor" d="M423.5 0C339.5.3 272 69.5 272 153.5V224H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48h-48v-71.1c0-39.6 31.7-72.5 71.3-72.9 40-.4 72.7 32.1 72.7 72v80c0 13.3 10.7 24 24 24h32c13.3 0 24-10.7 24-24v-80C576 68 507.5-.3 423.5 0zM264 392c0 22.1-17.9 40-40 40s-40-17.9-40-40v-48c0-22.1 17.9-40 40-40s40 17.9 40 40v48z"></path>
@@ -104,10 +93,10 @@ function generatePlace(place) {
 		<article class="manual table column">
 			<label>Manual control</label>
 			<div class="item table row wrap">
-				<button class="start secondary" onclick="confirm('Do you really want to turn the roulette ON (${place.name})?') && API.toggleState(STATE.UP, '${place.host}')" title="Send signal to start the roulette manually">Start</button>
-				<button class="stop secondary" onclick="confirm('Do you really want to turn the roulette OFF (${place.name})?') && API.toggleState(STATE.DOWN, '${place.host}')" title="Send signal to stop the roulette manually">Stop</button>
-				<button class="apply primary" onclick="confirm('Do you really want to save all changed values (${place.name})?') && saveChanges('${place.name}')" title="Save changes">Apply</button>
-				<button class="reload secondary" onclick="updatePlaceData('${place.name}')" title="Save changes">Reload</button>
+				<button class="start secondary" onclick="confirm('Do you really want to turn the roulette ON (${PLACE.config.NAME})?') && API.toggleState(STATE.UP, '${PLACE.host}')" title="Send signal to start the roulette manually">Start</button>
+				<button class="stop secondary" onclick="confirm('Do you really want to turn the roulette OFF (${PLACE.config.NAME})?') && API.toggleState(STATE.DOWN, '${PLACE.host}')" title="Send signal to stop the roulette manually">Stop</button>
+				<button class="apply primary" onclick="confirm('Do you really want to save all changed values (${PLACE.config.NAME})?') && saveChanges('${PLACE.config.NAME}')" title="Save changes">Apply</button>
+				<button class="reload secondary" onclick="updatePlaceData('${PLACE.config.NAME}')" title="Save changes">Reload</button>
 			</div>
 		</article>
 	</section>`);
@@ -115,9 +104,8 @@ function generatePlace(place) {
 	return node;
 }
 
-async function saveChanges(name) {
-	const place = PLACES.find(e => e.name == name);
-	const section = JL(`section[data-name="${place.name}"]`);
+async function saveChanges() {
+	const section = JL(`section[data-name="${PLACE.config.NAME}"]`);
 
 	const start = JL(section, "input.start").value.split(":");
 	const stop = JL(section, "input.stop").value.split(":");
@@ -127,53 +115,46 @@ async function saveChanges(name) {
 
 	if(locked && isNaN(lockState)) return alert(`Invalid 'lockState': You have to select locked state before locking it!`);
 
-	place.config.START.h = +start[0];
-	place.config.START.m = +start[1];
+	PLACE.config.START.h = +start[0];
+	PLACE.config.START.m = +start[1];
 
-	place.config.STOP.h = +stop[0];
-	place.config.STOP.m = +stop[1];
+	PLACE.config.STOP.h = +stop[0];
+	PLACE.config.STOP.m = +stop[1];
 
-	place.config.DELAY = +delay * 1000;
-	place.config.LOCKED = locked ? lockState : null;
+	PLACE.config.DELAY = +delay * 1000;
+	PLACE.config.LOCKED = locked ? lockState : null;
 
-	const res = await API.updateConfig(place.config, place.host); console.log(res);
-	place.config = res.config;
-	place.state = res.state;
-	updatePlaceElement(place);
+	const res = await API.updateConfig(PLACE.config); console.log(res);
+	PLACE.config = res.config;
+	PLACE.state = res.state;
+	updatePlaceElement();
 }
 
 function updatePlaces() {
 	JL("main").innerHTML = "";
-
-	for(const place of PLACES) {
-		JL("main").appendChild(generatePlace(place));
-	}
+	JL("main").appendChild(generatePlace());
 }
 
-async function updatePlaceData(name) {
-	const place = PLACES.find(e => e.name == name);
+async function updatePlaceData() {
+	const res = await API.getConfig(); console.log(res);
+	PLACE.config = res.config;
+	PLACE.state = res.state;
 
-	const res = await API.getConfig(place.host); console.log(res);
-	place.config = res.config;
-	place.state = res.state;
-
-	updatePlaceElement(place);
+	updatePlaceElement();
 }
 
-function updatePlaceElement(place) {
-	const section = JL(`section[data-name="${place.name}"]`);
+function updatePlaceElement() {
+	const section = JL(`section[data-name="${PLACE.config.NAME}"]`);
 
 	section.replaceWith(generatePlace(place));
 }
 
 async function loadConfig() {
-	for(const place of PLACES) {
-		console.log(`Loading configuration of ${place.name}...`);
-		const res = await API.getConfig(place.host);
-		place.config = res.config;
-		place.state = res.state;
-		console.log("Configuration loaded", place);
-	}
+	console.log(`Loading configuration...`);
+	const res = await API.getConfig();
+	PLACE.config = res.config;
+	PLACE.state = res.state;
+	console.log("Configuration loaded", PLACE);
 }
 
 main();
